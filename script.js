866e3e8b6885c2b3378b88dfcc8c2084fbea8155
Maria Isabel <mibalbuquerque@outlook.com>
2023-01-21 18:57:03 -0300

    innitial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('#form-habits')
+
+const nlwSetup = new NLWSetup(form);
+
+const button = document.querySelector('header button')
+
+button.addEventListener('click', addHabit)
+form.addEventListener("change", saveChange)
+
+
+
+function addHabit() {
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+let itsNewDay = nlwSetup.dayExists(today)
+    if (itsNewDay === true) {
+        alert("🛑 Dia já incluso 🛑")
+        return
+    }else {
+        nlwSetup.addDay(today)
+        alert("Dia incluído com sucesso ✅")
+    }
+
+
+
+}
+
+function saveChange() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
